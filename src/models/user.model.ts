b0de5c7452c004cb0cad5da8c2a2a65c40f05640
Prashant 	 Sharma <prashant.sharma@Prashant-Sharma.local>
2022-09-08 16:31:45 +0530

    loopback crud api memory connector

---
+++ src/models/user.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    // generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+  })
+  middleName?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'string',
+  })
+  phoneNumber?: string;
+
+  @property({
+    type: 'string',
+  })
+  email?: string;
+
+  @property({
+    type: 'string',
+  })
+  address?: string;
+
+  @property({
+    type: 'number',
+  })
+  role?: number;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
